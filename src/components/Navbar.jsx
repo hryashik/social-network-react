2b4da27a73f1c9f8c736b8b507ab959cd0f53737
Dmitriy <tsukikohaya@gmail.com>
2022-05-23 19:42:12 +0300

    Create project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+   return(
+      <nav className="nav">
+         <div><a href="http://localhost:3000/">Profile</a></div>
+         <div><a href="http://localhost:3000/">Messages</a></div>
+         <div><a href="http://localhost:3000/">News</a></div>
+         <div><a href="http://localhost:3000/">Music</a></div>
+         <div><a href="http://localhost:3000/">Settings</a></div>
+      </nav>
+   )
+}
+
+export default Navbar
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+function Profile() {
+   return (
+      <div className="content">
+         {/*<img*/}
+         {/*   src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"*/}
+         {/*   alt=""/>*/}
+         <div className="profile-image"></div>
+         <div>
+            Ava + description
+         </div>
+         <div>
+            My posts
+            <div>
+               New post
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Profile;
